1e4e3d1b0d833d2a2191283cc8f8241c6c879a73
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-02-22 02:15:37 +0600

    Codes of day 1

---
+++ main.py
@@ -0,0 +1,56 @@
+#print and variables 
+
+print("This is tanjumul starting the amazing world of python programming , excited !")
+
+#strings
+your_name = "Tanjumul"
+
+food = "Pasta"
+
+email = "Tanjum101@fake.com"
+
+print(f"your information is {your_name}, {email}")
+
+#integers
+
+age = 25 
+quantity = 105 
+print(f"your age is {age}")
+
+print(f"You class with {quantity} students")
+
+
+#float 
+
+price = 10.33
+cgpa = 3.54
+
+distance = 1.2 
+
+print(f"Your price of the food is {price} , your currnt cgpa is {cgpa} , Your department's distance from your university gate is {distance}km")
+
+
+#boolean 
+
+is_student = True
+
+that_item = False
+# if is_student:
+#     print("You are a student")
+
+# else: 
+#     print("You are not")
+
+
+if that_item: 
+    print("The iteam you are looking is available")
+
+else : 
+    print("That iteam you are looking is NOT available")
+    
+    
+
+
+tanju = "my name is Tanjumul"
+
+print(f"SO i share a thing{tanju}")
